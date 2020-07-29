7fb1f6d1e043d35647d65cfa046475da3f4b0497
unknown <jayson.mulwa@gmail.com>
2020-07-30 01:08:43 +0300

    Forms

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -16,11 +16,48 @@ const JumboComponentLazy = lazy(() =>
 class HomeComponent extends Component {
 
 	state = {
-		paycode: [],
 
-		amount: [],
+		fname: '',
+
+		idno: '',
+
+		kra: '',
+
+		position: '',
+
+		dpt: '',
+
+		payroll: '',
+
+		cname: '',
+
+		caddr: '',
+
 
 		values: [],
+
+
+
+		paycode_e: [],
+
+		amount_e: [],
+
+		values_e: [],
+
+
+		paycode_d: [],
+
+		amount_d: [],
+
+		values_d: [],
+
+
+		paycode_t: [],
+
+		amount_t: [],
+
+		values_t: [],
+
 	};
 
 onChange = e =>{
@@ -28,58 +65,172 @@ onChange = e =>{
 
 };
 
-createUI(){
-     return this.state.values.map((el, i) => 
-         <div key={i}>
 
-         	 <FormGroup>
-    	    
-			<Fade in={true} out={true} tag="h5" className="mt-3">
-					<Row>
-
-
-						<Col md="5"><Input type="paycode" name="paycode" id="paycode" placeholder="Item" onChange={this.handleChange.bind(this, i)} /> </Col>
-
-						<Col md="5"><Input type="amount" name="amount" id="amount" placeholder="Amount" onChange={this.onChange}/> </Col>
-
-						<Col md="2"><Button type='button' value='remove' onClick={this.removeClick.bind(this, i)}>Remove</Button> </Col>
-
-												
-
-
-					</Row>
-			</Fade>
-
-			 </FormGroup>
-
-         </div>          
-     )
-  }
-
-handleChange(i, event) {
-     let values = [...this.state.values];
+ /*let values = [...this.state.values];
      values[i] = event.target.value;
-     this.setState({ values });
-  }
-  
-  addClick(){
-    this.setState(prevState => ({ values: [...prevState.values, '']}))
-  }
-  
-  removeClick(i){
-     let values = [...this.state.values];
-     values.splice(i,1);
-     this.setState({ values });
-  }
+	 this.setState({ values });*/
+	 
+handleEarningsPaycodeChange(i, event) {
+    
+	 let paycode_e = [...this.state.paycode_e];
+     paycode_e[i] = event.target.value;
+     this.setState({ paycode_e });
+}
+
+handleEarningsAmountChange(i, event) {
+	
+	let amount_e = [...this.state.amount_e];
+	amount_e[i] = event.target.value;
+	this.setState({ amount_e });
+}
+handleDeductionsPaycodeChange(i, event) {
+    
+	let paycode_d = [...this.state.paycode_d];
+	paycode_d[i] = event.target.value;
+	this.setState({ paycode_d });
+}
+
+handleDeductionsAmountChange(i, event) {
+   
+   let amount_d = [...this.state.amount_d];
+   amount_d[i] = event.target.value;
+   this.setState({ amount_d });
+}
+handleTaxPaycodeChange(i, event) {
+    
+	let paycode_t = [...this.state.paycode_t];
+	paycode_t[i] = event.target.value;
+	this.setState({ paycode_t });
+}
+
+handleTaxAmountChange(i, event) {
+   
+   let amount_t = [...this.state.amount_t];
+   amount_t[i] = event.target.value;
+   this.setState({ amount_t });
+}
+
+	addClick(){
+		this.setState(prevState => ({ values: [...prevState.values, '']}))
+	}
+	addClick_e(){
+		this.setState(prevState => ({ values_e: [...prevState.values_e, '']}))
+	}
+	addClick_d(){
+		this.setState(prevState => ({ values_d: [...prevState.values_d, '']}))
+	}
+	addClick_t(){
+		this.setState(prevState => ({ values_t: [...prevState.values_t, '']}))
+	}
+	
+	removeClick(i){
+		let values = [...this.state.values];
+		values.splice(i,1);
+		this.setState({ values });
+	}
+	removeClick_e(i){
+		let values_e = [...this.state.values_e];
+		values_e.splice(i,1);
+		this.setState({ values_e });
+	}
+	removeClick_d(i){
+		let values_d = [...this.state.values_d];
+		values_d.splice(i,1);
+		this.setState({ values_d });
+	}
+	removeClick_t(i){
+		let values_t = [...this.state.values_t];
+		values_t.splice(i,1);
+		this.setState({ values_t });
+	}
 
 handleSubmit = (event) => {
 
-
-    alert('A name was submitted: ' + this.state.values.join(', '));
+	alert(this.state.fname + this.state.cname + this.state.caddr);
+	alert('A name was submitted: ' + this.state.paycode_e.join(', ') + ' ... ' + this.state.amount_e.join(', ') );
+	alert('A name was submitted: ' + this.state.paycode_d.join(', ') + ' ... ' + this.state.amount_d.join(', ') );
+	alert('A name was submitted: ' + this.state.paycode_t.join(', ') + ' ... ' + this.state.amount_t.join(', ') );
     event.preventDefault();
     
 
   };
+
+
+
+  createEarningsUI(){
+	return this.state.values_e.map((el, i) => 
+		<div key={i}>
+
+			 <FormGroup>
+		   
+		   <Fade in={true} out={true} tag="h5" className="mt-3">
+				   <Row>
+
+
+					   <Col md="5"><Input type="paycode" name="paycode" id="paycode" placeholder="Item" onChange={this.handleEarningsPaycodeChange.bind(this, i)} /> </Col>
+
+					   <Col md="5"><Input type="amount" name="amount" id="amount" placeholder="Amount" onChange={this.handleEarningsAmountChange.bind(this, i)}/> </Col>
+
+					   <Col md="2"><Button type='button' value='remove' onClick={this.removeClick_e.bind(this, i)}>Remove</Button> </Col>
+
+				   </Row>
+		   </Fade>
+
+			</FormGroup>
+
+		</div>          
+	)
+ }
+
+ createDeductionsUI(){
+	return this.state.values_d.map((el, i) => 
+		<div key={i}>
+
+			 <FormGroup>
+		   
+		   <Fade in={true} out={true} tag="h5" className="mt-3">
+				   <Row>
+
+
+					   <Col md="5"><Input type="paycode" name="paycode" id="paycode" placeholder="Item" onChange={this.handleDeductionsPaycodeChange.bind(this, i)} /> </Col>
+
+					   <Col md="5"><Input type="amount" name="amount" id="amount" placeholder="Amount" onChange={this.handleDeductionsAmountChange.bind(this, i)}/> </Col>
+
+					   <Col md="2"><Button type='button' value='remove' onClick={this.removeClick_d.bind(this, i)}>Remove</Button> </Col>
+
+				   </Row>
+		   </Fade>
+
+			</FormGroup>
+
+		</div>          
+	)
+ }
+
+ createTaxUI(){
+	return this.state.values_t.map((el, i) => 
+		<div key={i}>
+
+			 <FormGroup>
+		   
+		   <Fade in={true} out={true} tag="h5" className="mt-3">
+				   <Row>
+
+
+					   <Col md="5"><Input type="paycode" name="paycode" id="paycode" placeholder="Item" onChange={this.handleTaxPaycodeChange.bind(this, i)} /> </Col>
+
+					   <Col md="5"><Input type="amount" name="amount" id="amount" placeholder="Amount" onChange={this.handleTaxAmountChange.bind(this, i)}/> </Col>
+
+					   <Col md="2"><Button type='button' value='remove' onClick={this.removeClick_t.bind(this, i)}>Remove</Button> </Col>
+
+				   </Row>
+		   </Fade>
+
+			</FormGroup>
+
+		</div>          
+	)
+ }
 
 	render() {
 			return (
@@ -93,35 +244,28 @@ handleSubmit = (event) => {
 						        <Col md={{ size: 8, offset: 2 }}>
 
 								<Card body>
-								<CardTitle>Company Details</CardTitle>
+								<CardTitle id="Company">Company Details</CardTitle>
 
 
 
 									<Form onSubmit={this.handleSubmit}>
 
-										
-
-
-
-
 								      <FormGroup>
 								       
-
 
 									        <Row>
 
 
-									        <Col md="12" className="my-2"><Input type="paycode" name="paycode" id="paycode" placeholder="Company Name" onChange={this.onChange}/> </Col>
+									        <Col md="12" className="my-2"><Input type="text" name="cname" id="cname" placeholder="Company Name" onChange={this.onChange}/> </Col>
 
-									        <Col md="12" className="my-2"><Input type="amount" name="amount" id="amount" placeholder="Company Address" onChange={this.onChange}/> </Col>
+									        <Col md="12" className="my-2"><Input type="text" name="caddr" id="caddr" placeholder="Company Address" onChange={this.onChange}/> </Col>
 
 									        </Row>
 
 								      </FormGroup>
 								      		
-								      		<Button value="add more" onClick={this.addClick.bind(this)}>Add</Button> 
-
-								      		<Button className="mx-4" type="submit">Submit</Button>
+								      		
+								      		<Button type="submit">Submit</Button>
 
 								      
 								    </Form>
@@ -146,11 +290,6 @@ handleSubmit = (event) => {
 
 									<Form onSubmit={this.handleSubmit}>
 
-										
-
-
-
-
 								      <FormGroup>
 								       
 
@@ -158,15 +297,17 @@ handleSubmit = (event) => {
 									        <Row>
 
 
-									        <Col md="12" className="my-2"><Input type="paycode" name="paycode" id="paycode" placeholder="Full Name" onChange={this.onChange}/> </Col>
+									        <Col md="12" className="my-2"><Input type="text" name="fname" id="fname" placeholder="Full Name" onChange={this.onChange}/> </Col>
 
-									        <Col md="12" className="my-2"><Input type="amount" name="amount" id="amount" placeholder="ID Number" onChange={this.onChange}/> </Col>
+									        <Col md="12" className="my-2"><Input type="text" name="idno" id="ino" placeholder="ID Number" onChange={this.onChange}/> </Col>
 
-											<Col md="12" className="my-2"><Input type="paycode" name="paycode" id="paycode" placeholder="KRA PIN" onChange={this.onChange}/> </Col>
+											<Col md="12" className="my-2"><Input type="text" name="kra" id="kra" placeholder="KRA PIN" onChange={this.onChange}/> </Col>
 
-									        <Col md="12" className="my-2"><Input type="amount" name="amount" id="amount" placeholder="Position" onChange={this.onChange}/> </Col>
+									        <Col md="12" className="my-2"><Input type="text" name="position" id="position" placeholder="Position" onChange={this.onChange}/> </Col>
 
-											<Col md="12" className="my-2"><Input type="amount" name="amount" id="amount" placeholder="Department" onChange={this.onChange}/> </Col>
+											<Col md="12" className="my-2"><Input type="text" name="dpt" id="dpt" placeholder="Department" onChange={this.onChange}/> </Col>
+
+											<Col md="12" className="my-2"><Input type="text" name="payroll" id="payroll" placeholder="Payroll Number" onChange={this.onChange}/> </Col>
 
 									      
 
@@ -175,16 +316,10 @@ handleSubmit = (event) => {
 
 									        </Row>
 
-									        
-
-
-
-
 								      </FormGroup>
 								      		
-								      		<Button value="add more" onClick={this.addClick.bind(this)}>Add</Button> 
-
-								      		<Button className="mx-4" type="submit">Submit</Button>
+								      		
+								      		<Button type="submit">Submit</Button>
 
 								      
 								    </Form>
@@ -209,40 +344,11 @@ handleSubmit = (event) => {
 
 									<Form onSubmit={this.handleSubmit}>
 
-										
 
-
-
-
-								      <FormGroup>
-								       
-
-
-									        <Row>
-
-
-									        <Col md="5"><Input type="paycode" name="paycode" id="paycode" placeholder="Item" onChange={this.onChange}/> </Col>
-
-									        <Col md="5"><Input type="amount" name="amount" id="amount" placeholder="Amount" onChange={this.onChange}/> </Col>
-
-									        <Col md="2"><Button>Remove</Button> </Col>
-
-									        
-
-
-									        </Row>
-
-									        
-
-
-
-
-								      </FormGroup>
-
-								      {this.createUI()} 
+								      {this.createEarningsUI()} 
 
 								      		
-								      		<Button value="add more" onClick={this.addClick.bind(this)}>Add</Button> 
+								      		<Button value="add more" onClick={this.addClick_e.bind(this)}>Add</Button> 
 
 								      		<Button className="mx-4" type="submit">Submit</Button>
 
@@ -274,35 +380,10 @@ handleSubmit = (event) => {
 
 
 
-								      <FormGroup>
-								       
-
-
-									        <Row>
-
-
-									        <Col md="5"><Input type="paycode" name="paycode" id="paycode" placeholder="Item" onChange={this.onChange}/> </Col>
-
-									        <Col md="5"><Input type="amount" name="amount" id="amount" placeholder="Amount" onChange={this.onChange}/> </Col>
-
-									        <Col md="2"><Button>Remove</Button> </Col>
-
-									        
-
-
-									        </Row>
-
-									        
-
-
-
-
-								      </FormGroup>
-
-								      {this.createUI()} 
+								      {this.createDeductionsUI()} 
 
 								      		
-								      		<Button value="add more" onClick={this.addClick.bind(this)}>Add</Button> 
+								      		<Button value="add more" onClick={this.addClick_d.bind(this)}>Add</Button> 
 
 								      		<Button className="mx-4" type="submit">Submit</Button>
 
@@ -329,16 +410,8 @@ handleSubmit = (event) => {
 
 									<Form onSubmit={this.handleSubmit}>
 
-										
-
-
-
-
-								      <FormGroup>
-								       
-
-
-									        <Row>
+									 {/* <FormGroup>
+									  <Row>
 
 
 									        <Col md="5"><Input type="paycode" name="paycode" id="paycode" placeholder="Item" onChange={this.onChange}/> </Col>
@@ -347,22 +420,14 @@ handleSubmit = (event) => {
 
 									        <Col md="2"><Button>Remove</Button> </Col>
 
-									        
-
-
 									        </Row>
 
-									        
+									 </FormGroup>*/}
 
-
-
-
-								      </FormGroup>
-
-								      {this.createUI()} 
+								      {this.createTaxUI()} 
 
 								      		
-								      		<Button value="add more" onClick={this.addClick.bind(this)}>Add</Button> 
+								      		<Button value="add more" onClick={this.addClick_t.bind(this)}>Add</Button> 
 
 								      		<Button className="mx-4" type="submit">Submit</Button>
 
